e435a0c3043ae2563c69a0ce0183f10e80ca4853
Aman O <amanclct@gmail.com>
2025-05-07 18:38:46 +0100

    Add basic CSS for UI

---
+++ portfolio-ui/src/components/StartMenu.tsx
@@ -0,0 +1,28 @@
+function StartMenu() {
+  return (
+    <>
+      <div className="root-container">
+        <div className="header">Aman Ottakandathil</div>
+        <div className="container">
+          <div className="elem big">Resume</div>
+          <div className="elem small">Projects</div>
+          <div className="elem big">Education</div>
+          <div className="elem small">LinkedIn</div>
+          <div className="elem big">Github</div>
+          <div className="elem big">Time</div>
+          <div className="elem big"></div>
+          <div className="elem small"></div>
+          <div className="elem big"></div>
+          <div className="elem small"></div>
+          <div className="elem big"></div>
+          <div className="elem big"></div>
+          <div className="elem big"></div>
+          <div className="elem small"></div>
+          <div className="elem small"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default StartMenu;
