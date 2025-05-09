0f49cea214ad75417a799ebef8f5b228c913290a
Aman O <amanclct@gmail.com>
2025-05-09 15:34:42 +0100

    Add sub sections and fix text formatting

--- portfolio-ui/src/components/StartMenu.tsx
+++ portfolio-ui/src/components/StartMenu.tsx
@@ -5,22 +5,36 @@ function StartMenu() {
     <>
       <div className="container">
         <Header />
-        <div className="grid">
-          <div className="elem big">Resume</div>
-          <div className="elem small">Projects</div>
-          <div className="elem big">Education</div>
-          <div className="elem small">LinkedIn</div>
-          <div className="elem big">Github</div>
-          <div className="elem big">Time</div>
-          <div className="elem big"></div>
-          <div className="elem small"></div>
-          <div className="elem big"></div>
-          <div className="elem small"></div>
-          <div className="elem big"></div>
-          <div className="elem big"></div>
-          <div className="elem big"></div>
-          <div className="elem small"></div>
-          <div className="elem small"></div>
+        <div className="sections-container">
+          <div className="section-grid">
+            <h2 className="section-title"></h2>
+            <div className="elem big">About Me</div>
+            <div className="two-elem-grid">
+              <div className="elem small">LinkedIn</div>
+              <div className="elem small">GitHub</div>
+            </div>
+            <div className="elem big">CV</div>
+          </div>
+          <div className="section-grid">
+            <h2 className="section-title">Projects</h2>
+            <div className="elem big">Leap Analysis</div>
+            <div className="elem big">Distributed Tracing Framework</div>
+            <div className="two-elem-grid">
+              <div className="elem small">Terminal Text Editor</div>
+              <div className="elem small">Project Management Website</div>
+            </div>
+          </div>
+          <div className="section-grid">
+            <h2 className="section-title">Work Experience</h2>
+            <div className="elem big">Dexlock Technologies</div>
+          </div>
+          <div className="section-grid">
+            <h2 className="section-title">Education</h2>
+            <div className="elem big">
+              <div className="elem-text">The University Of Leeds</div>
+            </div>
+            <div className="elem big">Government Model Engineering College</div>
+          </div>
         </div>
       </div>
     </>
