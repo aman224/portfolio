62b68518aa5499e49f494b090e66f559aff89cc4
Aman O <amanclct@gmail.com>
2025-05-26 12:17:19 +0100

    Add quicklinks to homescreen

--- portfolio-ui/src/components/Home.tsx
+++ portfolio-ui/src/components/Home.tsx
@@ -1,14 +1,18 @@
 import HomeSocials from "./HomeSocials";
+import QuickLinks from "./QuickLInks";
 
 function Home() {
   return (
     <div className="home-content">
-      <h1 className="header">Hey, I'm Aman</h1>
-      <p className="header-subtext">
-        A Software Engineer passionate about building and deploying web
-        applications on AWS and Azure. I excel in agile teams to deliver
-        solutions, resolve complex challenges, and optimize infrastructure.
-      </p>
+      <div className="header">
+        <h1 className="header-title">Hey, I'm Aman</h1>
+        <p className="header-subtext">
+          A Software Engineer passionate about building and deploying web
+          applications on AWS and Azure. I excel in agile teams to deliver
+          solutions, resolve complex challenges, and optimize infrastructure.
+        </p>
+      </div>
+      <QuickLinks />
       <HomeSocials />
     </div>
   );
---
+++ portfolio-ui/src/components/QuickLInks.tsx
@@ -0,0 +1,11 @@
+function QuickLinks() {
+  return (
+    <div className="quick-links">
+      <div className="quick-links-item">View my Projects</div>
+      <div className="quick-links-item">See my Work Experience</div>
+      <div className="quick-links-item">Contact Me</div>
+    </div>
+  );
+}
+
+export default QuickLinks;
