bcf01a3a5759ed6956e541ac7b9ac2577ed226b1
Aman O <amanclct@gmail.com>
2025-05-28 20:02:18 +0100

    Update UI, home colour and spacing

--- portfolio-ui/src/components/QuickLInks.tsx
+++ portfolio-ui/src/components/QuickLInks.tsx
@@ -1,8 +1,14 @@
 function QuickLinks() {
   return (
     <div className="quick-links">
-      <div className="quick-links-item">View my Projects</div>
-      <div className="quick-links-item">See my Work Experience</div>
+      <div className="quick-links-item">
+        <div className="quick-links-text">
+          <p>See my Work Experience</p>
+        </div>
+      </div>
+      <div className="quick-links-item">
+        <p>View my Projects</p>
+      </div>
     </div>
   );
 }
