f042ce8024714a4402c9c49c08afd2a9c04426d0
Aman O <amanclct@gmail.com>
2025-05-28 19:05:16 +0100

    Add new dark background image, fix image loading issue

--- portfolio-ui/src/components/Home.tsx
+++ portfolio-ui/src/components/Home.tsx
@@ -4,16 +4,23 @@ import QuickLinks from "./QuickLInks";
 function Home() {
   return (
     <div className="home-container">
-      <div className="header">
-        <h1 className="header-title">Hey, I'm Aman</h1>
-        <p className="header-subtext">
-          A Software Engineer passionate about building and deploying web
-          applications on AWS and Azure. I excel in agile teams to deliver
-          solutions, resolve complex challenges, and optimize infrastructure.
-        </p>
+      <div className="home-primary-section">
+        <div className="header">
+          <h1 className="header-title">Hey, I'm Aman</h1>
+          <div className="header-subtext">
+            <p>
+              A Software Engineer passionate about building and deploying web
+              applications on AWS and Azure. I excel in agile teams to deliver
+              solutions, resolve complex challenges, and optimize
+              infrastructure.
+            </p>
+          </div>
+        </div>
       </div>
-      <QuickLinks />
-      <HomeSocials />
+      <div className="home-secondary-section">
+        <QuickLinks />
+        <HomeSocials />
+      </div>
     </div>
   );
 }
