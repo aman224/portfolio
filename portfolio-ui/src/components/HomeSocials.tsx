c8cf7b325fc456ead6c3c879a688a4fe2e0aac56
Aman O <amanclct@gmail.com>
2025-05-29 17:46:56 +0100

    Prefetch background images, improve socials UI

--- portfolio-ui/src/components/HomeSocials.tsx
+++ portfolio-ui/src/components/HomeSocials.tsx
@@ -7,20 +7,20 @@ function HomeSocials() {
   const iconSize = "medium";
 
   return (
-    <div>
-      <div className="home-socials">
-        <div className="home-socials-item">
-          <LinkedInIcon fontSize={iconSize} />
-        </div>
-        <div className="home-socials-item">
-          <GitHubIcon fontSize={iconSize} />
-        </div>
-        <div className="home-socials-item">
-          <EmailRoundedIcon fontSize={iconSize} />
-        </div>
-        <div className="home-socials-item">
-          <InstagramIcon fontSize={iconSize} />
-        </div>
+    <div className="home-socials">
+      <div className="home-socials-item">
+        <LinkedInIcon fontSize={iconSize} />
+        <h3 className="home-socials-item-text">LinkedIn</h3>
+      </div>
+      <div className="home-socials-item">
+        <GitHubIcon fontSize={iconSize} />
+        <h3 className="home-socials-item-text">GitHub</h3>
+      </div>
+      <div className="home-socials-item">
+        <EmailRoundedIcon fontSize={iconSize} />
+      </div>
+      <div className="home-socials-item">
+        <InstagramIcon fontSize={iconSize} />
       </div>
     </div>
   );
